050f18bb82d58785fe857ab6ce575990f5857f3a
SarasiNishala <sarasinishala@gmail.com>
2023-11-08 14:44:18 +0530

    ui created

--- src/main/java/lk/ijse/zoocity/controller/LoginFormController.java
+++ src/main/java/lk/ijse/zoocity/controller/LoginFormController.java
@@ -1,4 +1,13 @@
 package lk.ijse.zoocity.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
 public class LoginFormController {
+    public TextField txtUsername;
+    public TextField txtPassword;
+
+    public void btnOnActionLogin(ActionEvent actionEvent) {
+
+    }
 }
